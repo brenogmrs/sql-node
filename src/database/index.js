fae7bcd9bc7c62f089d96054469d54bfea730f64
brenoaraujo1 <brenoaraujo@outlook.com.br>
2020-06-21 16:02:49 -0300

    relacionamentos, rotas e controllers

--- src/database/index.js
+++ src/database/index.js
@@ -4,15 +4,19 @@ const dBConfig = require('../config/database.config');
 
 const User = require('../models/User');
 const Address = require('../models/Address');
+const Tech = require('../models/Tech');
 
 
 const connection = new Sequelize(dBConfig);
 
 User.init(connection);
 Address.init(connection);
+Tech.init(connection);
+
 
 Address.associate(connection.models);
 User.associate(connection.models);
+Tech.associate(connection.models);
 
 
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const UserController = require('./controllers/UserController')
-const AddressController = require('./controllers/AddressController')
+const UserController = require('./controllers/UserController');
+const AddressController = require('./controllers/AddressController');
+const TechController = require('./controllers/TechController');
+const ReportController = require('./controllers/ReportController');
+
 
 const router = express.Router();
 
@@ -10,6 +13,10 @@ router.get('/users', UserController.index);
 router.post('/users/:user_id/addresses', AddressController.store);
 router.get('/users/:user_id/addresses', AddressController.index);
 
+router.post('/users/:user_id/techs', TechController.store);
+router.get('/users/:user_id/techs', TechController.index);
+router.delete('/users/:user_id/techs', TechController.delete);
 
+router.get('/report', ReportController.show);
 
 module.exports = router;
